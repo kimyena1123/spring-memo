d5c73f7340d74ec5ec27aedba18bec701eafe1a2
kimyena1123 <kyq104@gmail.com>
2024-08-11 20:49:45 +0900

    feat: 비밀번호 암호화(MD5 사용)

---
+++ src/main/java/com/kimyena/memo/common/EncryptUtils.java
@@ -0,0 +1,39 @@
+package com.kimyena.memo.common;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncryptUtils {
+
+    // 비밀번호 암호화
+    //hashing을 사용해 암호화 진행
+    //암호화는 회원가입 뿐만 아니라 로그인 시에도 사용된다. => 메소드로!
+    public static String md5(String message) { // message 비밀번호 받기
+        StringBuilder resultData = new StringBuilder();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+
+            //message를 바이트 단위로 바꿔줘야 한다.
+            byte[] bytes = message.getBytes();
+            md.update(bytes); // 이 안에서 message를 암호화한다.
+
+            // 그 결과를 bytes 배열 형태로 돌려준다.
+            //digest : 암호화된 결과임.
+            byte[] digest = md.digest();
+
+            //암호화 된 걸 문자열로
+            //=> 16진수 형태의 문자열로 변환
+            for(int i = 0; i < digest.length; i++) {
+                resultData.append(Integer.toHexString(digest[i] & 0xff)); // 비트연산
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+
+            e.printStackTrace();
+        }
+
+        return resultData.toString();
+    }
+
+}
--- src/main/java/com/kimyena/memo/user/bo/UserBO.java
+++ src/main/java/com/kimyena/memo/user/bo/UserBO.java
@@ -1,5 +1,6 @@
 package com.kimyena.memo.user.bo;
 
+import com.kimyena.memo.common.EncryptUtils;
 import com.kimyena.memo.user.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,10 @@ public class UserBO {
             String user_name,
             String user_email
     ){
-        return userDAO.insertUser(user_id, user_pw, user_name, user_email);
+        //비밀번호 암호화
+        String encryptPassword = EncryptUtils.md5(user_pw);
+
+        //넘겨줄 때 user_pw가 아닌 암호화된 encryptPassword로 넘겨준다.
+        return userDAO.insertUser(user_id, encryptPassword, user_name, user_email);
     }
 }
